0cc7fd72eec4949a6ee7759fac574064d2c2e90b
lorne <1991wangliang@gmail.com>
2026-01-28 15:23:23 +0800

    add flowgram

--- frontend/packages/flow-design/rslib.config.ts
+++ frontend/packages/flow-design/rslib.config.ts
@@ -1,5 +1,7 @@
 import { pluginReact } from '@rsbuild/plugin-react';
 import { defineConfig } from '@rslib/core';
+import { pluginLess } from '@rsbuild/plugin-less';
+import {pluginSass} from '@rsbuild/plugin-sass';
 import * as path from 'node:path';
 
 export default defineConfig({
@@ -23,5 +25,5 @@ export default defineConfig({
   output: {
     target: 'web',
   },
-  plugins: [pluginReact()],
+  plugins: [pluginReact(),pluginLess(),pluginSass()],
 });
--- frontend/packages/flow-design/src/pages/design-panel/tabs/flow.tsx
+++ frontend/packages/flow-design/src/pages/design-panel/tabs/flow.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import {DemoFixedLayout} from '@flow-engine/flowgram';
+
+const FlowDesignWapper: React.FC = () => {
+    return (
+        <div style={{ height: 'calc(100vh - 100px)',width:'100%',position:'relative'  }}>
+            <DemoFixedLayout />
+        </div>
+    );
+};
 
 
 export const TabFlow = ()=>{
 
     return (
-        <>flow</>
+        <FlowDesignWapper/>
     )
 }
