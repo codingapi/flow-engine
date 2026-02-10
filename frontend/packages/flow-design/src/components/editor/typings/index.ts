49075dadc51c2c19c38c338650545defa4c88aca
lorne <1991wangliang@gmail.com>
2026-02-10 10:47:18 +0800

    remove nothing code

--- frontend/packages/flow-design/src/components/editor/typings/index.ts
+++ frontend/packages/flow-design/src/components/editor/typings/index.ts
@@ -1,2 +1,1 @@
-export * from './json-schema';
 export * from './node';
--- frontend/packages/flow-design/src/components/editor/typings/node-type.ts
+++ frontend/packages/flow-design/src/components/editor/typings/node-type.ts
@@ -33,46 +33,6 @@ export type NodeType =
     // 触发
     "TRIGGER";
 
-
-export type NodeStrategyType =
-    // 节点审批意见策略
-    "AdviceStrategy" |
-    // 延迟策略配置
-    "DelayStrategy" |
-    // 错误触发策略配置(没有匹配到人时)
-    "ErrorTriggerStrategy" |
-    // 表单字段权限策略配置
-    "FormFieldPermissionStrategy"|
-    // 多人审批策略配置
-    "MultiOperatorAuditStrategy"|
-    // 节点标题策略配置
-    "NodeTitleStrategy"|
-    // 操作人配置策略
-    "OperatorLoadStrategy"|
-    // 记录合并策略配置
-    "RecordMergeStrategy"|
-    // 重新提交策略配置
-    "ResubmitStrategy"|
-    // 路由策略配置
-    "RouterStrategy"|
-    // 撤回策略
-    "RevokeStrategy"|
-    // 提交人与审批人一致配置
-    "SameOperatorAuditStrategy"|
-    // 子流程任务策略
-    "SubProcessStrategy"|
-    // 超时策略配置
-    "TimeoutStrategy"|
-    // 触发策略配置
-    "TriggerStrategy" |
-    // 条件分支策略
-    "ConditionBranchStrategy" |
-    // 并行分支策略
-    "ParallelBranchStrategy"|
-    // 包容分支策略
-    "InclusiveBranchStrategy";
-
-
 /**
  * 操作类型
  */
