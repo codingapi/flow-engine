1facac4e11570ea9eae77fb703a53a3642c6cb0b
lorne <1991wangliang@gmail.com>
2026-03-05 10:25:24 +0800

    update script design

--- frontend/packages/flow-pc/flow-pc-design/src/components/script/typings/index.ts
+++ frontend/packages/flow-pc/flow-pc-design/src/components/script/typings/index.ts
@@ -4,6 +4,10 @@ export const CUSTOM_SCRIPT = '@CUSTOM_SCRIPT';
 /** 脚本标题标记 */
 export const SCRIPT_TITLE = '@SCRIPT_TITLE';
 
+
+/** 脚本元数据标记 */
+export const SCRIPT_META = '@SCRIPT_META';
+
 /**
  * Groovy脚本类型枚举
  */
--- frontend/packages/flow-pc/flow-pc-design/tests/script/utils/convertor-node-title.test.ts
+++ frontend/packages/flow-pc/flow-pc-design/tests/script/utils/convertor-node-title.test.ts
@@ -6,6 +6,7 @@ import {
 import {
     NodeTitleVariableAdapter
 } from "@/components/script/services/variable/node-title";
+import {GroovyScriptConvertorUtil} from "@/components/script/utils/convertor";
 
 describe('NodeTitleGroovyConvertor', () => {
 
@@ -38,6 +39,7 @@ describe('NodeTitleGroovyConvertor', () => {
     describe('toExpression', () => {
         it('node title script to expression', () => {
             const script = `
+// @SCRIPT_TITLE 您有一条待办消息\${当前操作人ID}\${当前操作人ID}\${流程创建人}\${是否管理员}   
 def run(request){
 return "您有一条待办消息" + request.getOperatorId() + request.getOperatorId() + request.getCreatorName() + request.getIsFlowManager()
 }
@@ -59,7 +61,8 @@ def run(request){
 `
             const nodeTitleGroovyConvertor = new NodeTitleGroovyConvertor(script, nodeTitleVariableAdapter.getVariables());
             const result = nodeTitleGroovyConvertor.resetExpression('你有一条${当前操作人}的${流程标题}待办消息 【${当前节点}】')
-            expect(result).toEqual(script);
+            const title = GroovyScriptConvertorUtil.getScriptTitle(result);
+            expect(title).toEqual("你有一条${当前操作人}的${流程标题}待办消息 【${当前节点}】");
         });
     });
 });
