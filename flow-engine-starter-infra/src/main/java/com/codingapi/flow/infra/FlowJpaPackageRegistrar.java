8ae6cdabc3aa292ea41a6e94ac037d2ad679148e
lorne <1991wangliang@gmail.com>
2026-03-09 22:39:27 +0800

    add FlowJpaPackageRegistrar.java

--- flow-engine-starter-infra/src/main/java/com/codingapi/flow/infra/FlowJpaPackageRegistrar.java
+++ flow-engine-starter-infra/src/main/java/com/codingapi/flow/infra/FlowJpaPackageRegistrar.java
@@ -4,14 +4,16 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.lang.NonNull;
 
 public class FlowJpaPackageRegistrar
         implements ImportBeanDefinitionRegistrar  {
 
     @Override
     public void registerBeanDefinitions(
-            AnnotationMetadata metadata,
-            BeanDefinitionRegistry registry) {
-        AutoConfigurationPackages.register(registry, "com.codingapi.flow.infra");
+            @NonNull AnnotationMetadata metadata,
+            @NonNull BeanDefinitionRegistry registry) {
+        AutoConfigurationPackages.register(registry,
+                "com.codingapi.flow.infra");
     }
 }
